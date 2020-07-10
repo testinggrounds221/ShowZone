6a00e7eb4ca9dc2faf22af63be46f5e691e64c63
Ram <testinggrounds221@gmail.com>
2020-07-10 14:04:32 +0530

    Cards

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -6,9 +6,10 @@ function Card(props){
     backgroundImage: `url('${props.img? props.img:'' }')`,
     position:'relative',
     height:'300px',
-    width:'200px'
-  };
-  const imgStyleNoImg =  {
+    width:'200px',
+    backgroundRepeat: 'no-repeat',
+};
+const imgStyleNoImg =  {
     position:'relative',
     backgroundImage: 'url("https://raw.githubusercontent.com/testinggrounds221/ShowZone/master/i/ques.svg")', /* The image used */
     height: '300px',/* You must set a specified height */
@@ -16,19 +17,25 @@ function Card(props){
     backgroundPosition: 'center', /* Center the image */
     backgroundRepeat: 'no-repeat', /* Do not repeat the image */
     backgroundSize: 'cover' /* Resize the background image to cover the entire container */
-  }
-  const txtStyle = {
+}
+const txtStyle = {
     'width': '100%',
-    'height': '100%',
-    'backgroundColor': 'rgba(0,0,0,0.5)',
+    'height': 'auto',
+    'backgroundColor':'rgba(255, 255, 255, 0.5)',
+    'color': '#272E34',
+    'opacity': '0.9',
     'left':'0',
-    'top':'0',
+    'bottom':'0',
     'position': 'absolute',
     'zIndex': '10',
-    'paddingTop':'100px',
-    'opacity': '0.9'
-  }
-  return(
+    'paddingTop':'5px',
+    'fontWeight': 'bold',
+    'fontFamily': 'Alegreya SC',
+    'fontSize':'20px',
+    'paddingRight':'2px',
+    'paddingLeft':'2px',
+}
+return(
     // bg-pl-2
     <div className="shadow flex content-center">
     <div className="mx-auto">
